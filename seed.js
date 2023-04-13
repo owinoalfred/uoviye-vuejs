dcfb594eaba1d30ac006074ca7a3e88f892700f7
owinoalfred <ogzialfred@gmail.com>
2023-04-13 10:21:00 +0300

    adding the seeds.js

---
+++ seed.js
@@ -0,0 +1,9 @@
+const submissions = [{
+    id:1,
+    title: "Yellow Pail",
+    description: "On-demand sand and castly construction Expertise",
+    votes: 16,
+    avatar: "/images/shop.png",
+    sumbmissionImage: '/images/shopping-bag.png'
+},
+]
